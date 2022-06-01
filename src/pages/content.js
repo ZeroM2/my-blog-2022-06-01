2d709aea3b63da7fd5069e21733cacc8fea258d9
“ZeroM2” <Kemy21@naver.com>
2022-06-01 23:26:37 +0900

    콘텐트 페이지 추가, a테그 Link로 변경

---
+++ src/pages/content.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function content() {
+  return (
+    <main>
+      <title> content</title>
+      <h1>콘텐츠가 업로드 되는 곳입니다. </h1>
+    </main>
+  );
+}
+
+export default content;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
+import { Link } from "gatsby";
 import React from "react";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page!!!</title>
+
+      <div style={{ display: "flex", gap: 20 }}>
+        <Link href="/">홈</Link>
+        <Link href="/about">about</Link>
+        <Link href="/content">content</Link>
+      </div>
       <h1>Home !!!!</h1>
     </main>
   );
